1a4ca8ecd1a47dd008c68989170c27406b2ad60c
Hakyung Yun <87377304+hakyung4@users.noreply.github.com>
2021-11-01 13:24:50 -0500

    Add files via upload

--- Email automation/mail_auto.py
+++ Email automation/mail_auto.py
@@ -6,10 +6,10 @@ from email.message import EmailMessage
 EMAIL_ADDR = os.environ.get('EMAIL_ADDR')
 EMAIL_PASS = os.environ.get('EMAIL_PASS')
 
-contacts = ['klskoreanlanguage@gmail.com', 'angtal82@gmail.com', 'rtxcorp@nate.com', '7942ar@gmail.com', 'miracle00827@gmail.com']
+contacts = ['angtal82@gmail.com', 'rtxcorp@nate.com', '7942ar@gmail.com', 'miracle00827@gmail.com']
 
 msg = EmailMessage()
-msg['Subject'] = 'You'
+msg['Subject'] = '10/30 Saturday Class'
 msg['From'] = EMAIL_ADDR
 msg['To'] = ', '.join(contacts)
 
@@ -18,29 +18,29 @@ msg.add_alternative("""\
 <html>
 <body>
     <h3><b>안녕하세요!</b></h3>
-    <p>This is Peter Yun, this year's ESL 1 teacher. Since this week is halloween week, students are welcome to bring or put on whatever they want to wear; however, I highly encourage everyone to bring their own jacket in case we go outside for activities. I would appreiciate it if all students brought the following items to school:</p>
+    <p>This is Peter Yun, this year's ESL 1 teacher. Last Saturday, students learned about Point of View and how to identify them in stories. Students also had fun playing with Ddakji they made and Dalgona. I would appreiciate it if all students brought the following items to school:</p>
     <ul>
         <li>Pencils</li>
         <li>Eraser</li>
         <li>Water bottle</li>
     </ul>
-    <p>This Saturday, students will learn Point of view in stories and practice identifying it.</p>
+    <p>This Saturday, students will expand their knowledge of genres and venture out into fictional texts.</p>
     <p>--------------------------------------------------------------------------------------------------------------------------------------------------------</p>
     <ol>
-        <li>10:30-11:20-> <b>Point of view</b></li>
+        <li>10:30-11:20-> <b>Distinguishing Fiction from other genres</b></li>
         <ol>
-            <li>Point of view</li>
-            <li>Pronouns & Point of View</li>
+            <li>Fact or Fiction</li>
+            <li>Fiction or nonfiction?</li>
         </ol>
         <li>11:20-12:10-> <b>Exercise</b></li>
         <ol>
-            <li>Point of view in sentences</li>
-            <li>Practicing point of view</li>
+            <li>Vocabulary Practice: Fiction Genre</li>
+            <li>Simple Fiction Summary</li>
         </ol>
         <li>12:10-12:30-> <b>Snack</b></li>
         <ol>
-            <li>Ham & Cheese croissant sandwhich</li>
-            <li>Orange juice</li>
+            <li>dunkin donuts</li>
+            <li>Milk</li>
         </ol>
         <li>12:30-1:30-> <b>Collaborative activities</b></li>
         <ol>
